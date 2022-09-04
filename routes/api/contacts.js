ae1a9ff9add345786e9a5a840aa3799d591205fe
Nataliia Lyfar <lifarnatala@gmail.com>
2022-09-04 13:12:45 +0300

    refactoring code with mongoose

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,19 +2,21 @@ const express = require("express");
 const router = express.Router();
 
 const { contacts: ctrl } = require("../../controllers");
-const { validation, ctrlWrapper } = require("../../middlewares");
-const { contactSchema } = require("../../schemas");
+const { ctrlWrapper } = require("../../helpers");
+const { validation, isValidId } = require("../../middlewares");
+const { schemas } = require("../../models/contact");
 
-const validateMiddleware = validation(contactSchema);
 
 router.get("/", ctrlWrapper(ctrl.getListContacts));
 
-router.get("/:contactId", ctrlWrapper(ctrl.getContactById));
+router.get("/:id", isValidId, ctrlWrapper(ctrl.getContactById));
 
-router.post("/", validateMiddleware, ctrlWrapper(ctrl.addContact));
+router.post("/", validation(schemas.add), ctrlWrapper(ctrl.addContact));
 
-router.delete("/:contactId", ctrlWrapper(ctrl.removeContactById));
+router.put("/:id", isValidId, validation(schemas.add), ctrlWrapper(ctrl.updateContactById));
 
-router.put("/:contactId", validateMiddleware, ctrlWrapper(ctrl.updateContactById));
+router.patch("/:id/favorite", isValidId, validation(schemas.updateStatus), ctrlWrapper(ctrl.updateStatusContact));
+
+router.delete("/:id", isValidId, ctrlWrapper(ctrl.removeContactById));
 
 module.exports = router;
