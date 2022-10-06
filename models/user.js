1dca275a7bb8923f8e17a75f198ab31c7a4c6f9b
Nataliia Lyfar <lifarnatala@gmail.com>
2022-10-06 22:03:49 +0300

    fixed try catch above fs.rename in updateAvatar controller

--- models/user.js
+++ models/user.js
@@ -55,7 +55,6 @@ const joiRegisterSchema = Joi.object({
   password: Joi.string().required(),
   email: Joi.string().pattern(emailRegexp).required(),
   subscription: Joi.string(),
-  avatarURL: Joi.string(),
 });
 
 const joiLoginSchema = Joi.object({
