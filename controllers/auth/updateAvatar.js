19cc3f29956ff46839b9a4c0404ecf1edd9144c3
Nataliia Lyfar <lifarnatala@gmail.com>
2022-10-06 21:59:36 +0300

    fixed try catch above fs.rename in updateAvatar

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -6,26 +6,25 @@ const Jimp = require("jimp");
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 
 const updateAvatar = async (req, res) => {
-    const { path: tempUpload, originalname } = req.file;
-    const { _id: id } = req.user;
-    const avatarName = `${id}_${originalname}`;
-    
-    try{
-    const resultUpload = path.join(avatarsDir, avatarName);
+  const { path: tempUpload, originalname } = req.file;
+  const { _id: id } = req.user;
+  const avatarName = `${id}_${originalname}`;
+
+  const resultUpload = path.join(avatarsDir, avatarName);
+  try {
     await fs.rename(tempUpload, resultUpload);
-
-    const file = await Jimp.read(resultUpload);
-    await file.resize(250, 250).write(resultUpload);
-
-    const avatarURL = path.join("avatars", avatarName);
-
-    await User.findByIdAndUpdate(id, { avatarURL });
-
-    res.json({ avatarURL });
-    } catch (error) {
+  } catch (error) {
     await fs.unlink(tempUpload);
     throw error;
-    }
+  }
+  const file = await Jimp.read(resultUpload);
+  await file.resize(250, 250).write(resultUpload);
+
+  const avatarURL = path.join("avatars", avatarName);
+
+  await User.findByIdAndUpdate(id, { avatarURL });
+
+  res.json({ avatarURL });
 };
 
 module.exports = updateAvatar;
