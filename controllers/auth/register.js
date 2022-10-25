66ed026d40c1b3a82d2787b7de64c0b9ea206fbe
Nataliia Lyfar <lifarnatala@gmail.com>
2022-10-25 09:19:01 +0300

    fixed controllers

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -3,17 +3,21 @@ const { RequestError } = require("../../helpers");
 const { User } = require("../../models/user");
 
 const register = async (req, res) => {
-  const { name, email, password } = req.body;
+  const { email, password, subscription } = req.body;
   const user = await User.findOne({ email });
   if (user) {
     throw RequestError(409, `User with ${email} already exist`);
   }
   const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-  const result = await User.create({ name, email, password: hashPassword });
+  await User.create({
+    email,
+    password: hashPassword,
+    subscription,
+  });
   res.status(201).json({
     user: {
-      name: result.name,
-      email: result.email,
+      email,
+      subscription,
     },
   });
 };
